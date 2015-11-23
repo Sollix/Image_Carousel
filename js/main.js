e1b74df85cbef663b94f6133b3ba883f4a4d9569
Matt Kistler <Matt.A.Kistler@gmail.com>
2015-11-22 21:13:58 -0600

    updates, trying some stuff in the js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ outputs: img, bonus
 global var: current img index
 events: previous click, next click, drowdown vote
 */
+//global properties
 imgArray = ['image_1.jpg', 'image_2.jpg','image_3.jpg','image_4.jpg', 'image_5.jpg', 'image_6.jpg'];
 
 currentindex = 0;
 console.log(currentindex);
 
+//next and previous buttons
 $('#next').on('click',function(event){
 		if (currentindex < 5){
 		currentindex = currentindex + 1;
@@ -24,8 +26,19 @@ $('#prev').on('click',function(event){
 		if (currentindex > 0){
 		currentindex = currentindex - 1;
 		$('#image-to-vote-on').attr('src','images/' + imgArray[currentindex]);
-		$('#next').css('color','black');
 		}else{
 			$('#image-to-vote-on').attr('src','images/' + imgArray[0]);
 		}
 	});
+
+//image scoring
+
+var scoreArray = [];
+
+$('#your-vote').select(function(event){
+	scoreArray[currentindex]=$('#your-vote').val();
+	console.log(scoreArray);
+	//currentindex = currentindex + 1;
+	$('#image-to-vote-on').attr('src','images/' + imgArray[currentindex])
+}); 
+	
